ff15317fb5f822d455c45e95d86a04e85949e864
Amandeep Singh Arora <aman@dianomic.com>
2019-04-09 12:26:06 +0100

    Added new CNN based TF model for better digit recognition

--- classifier.cpp
+++ classifier.cpp
@@ -32,6 +32,7 @@ const vector<string> labels { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }
 
 int digit;
 float topn_prob=0.0;
+ofstream logfile;
 
 /**
  * Constructor for ImageClassifer class
@@ -52,44 +53,49 @@ ImageClassifier::~ImageClassifier()
  * Get top 'n' indices in output tensor that input tensor could possibly be representing
  */
 template<typename T>
-std::vector<int> GetTopNIndices(const T* data, int size, int n) {
-    std::vector<int> topn;
-    auto comp = [&](int i, int j) -> bool { return data[i] > data[j]; };
-    topn_prob=0.0;
-    for (int i = 0; i < size; i++) {
+std::vector<int> GetTopNIndices(const T* data, int size, int n)
+{
+	std::vector<int> topn;
+	auto comp = [&](int i, int j) -> bool { return data[i] > data[j]; };
+	topn_prob=0.0;
+	for (int i = 0; i < size; i++)
+	{
 		//Logger::getLogger()->info("i=%d, data[i]=%f", i, data[i]);
 		topn_prob=std::max(topn_prob, (float) data[i]);
-        topn.push_back(i);
-        std::push_heap(topn.begin(), topn.end(), comp);
-        if (topn.size() > n) {
-            std::pop_heap(topn.begin(), topn.end(), comp);
-            topn.pop_back();
-        }
-    }
-    std::sort_heap(topn.begin(), topn.end(), comp);
-    return topn;
+		topn.push_back(i);
+		std::push_heap(topn.begin(), topn.end(), comp);
+		if (topn.size() > n) {
+			std::pop_heap(topn.begin(), topn.end(), comp);
+			topn.pop_back();
+		}
+	}
+	std::sort_heap(topn.begin(), topn.end(), comp);
+	return topn;
 }
 
 /**
  * Get top 'n' digits the input tensor could possibly be representing
  */
-std::vector<std::string> GetTopN(TfLiteTensor* output, int n) {
-    std::vector<int> topn;
-    switch (output->type) {
-        case kTfLiteFloat32:
-            topn = GetTopNIndices<float>(output->data.f, output->dims->data[1], n);
-            break;
-        case kTfLiteUInt8:
-            topn = GetTopNIndices<uint8_t>(output->data.uint8, output->dims->data[1], n);
-            break;
-        default:
-            Logger::getLogger()->error("Unable to process output tensor flow data");
-    }
-    std::vector<std::string> topn_labels(topn.size());
-    for (int i = 0; i < topn.size(); i++) {
-        if (topn[i] < labels.size()) topn_labels[i] = labels[topn[i]];
-    }
-    return topn_labels;
+std::vector<std::string> GetTopN(TfLiteTensor* output, int n)
+{
+	std::vector<int> topn;
+	switch (output->type)
+	{
+		case kTfLiteFloat32:
+			topn = GetTopNIndices<float>(output->data.f, output->dims->data[1], n);
+			break;
+		case kTfLiteUInt8:
+			topn = GetTopNIndices<uint8_t>(output->data.uint8, output->dims->data[1], n);
+			break;
+		default:
+			Logger::getLogger()->error("Unable to process output tensor flow data");
+	}
+	std::vector<std::string> topn_labels(topn.size());
+	for (int i = 0; i < topn.size(); i++)
+	{
+		if (topn[i] < labels.size()) topn_labels[i] = labels[topn[i]];
+	}
+	return topn_labels;
 }
 
 /**
@@ -97,49 +103,49 @@ std::vector<std::string> GetTopN(TfLiteTensor* output, int n) {
  */
 int ImageClassifier::identifyDigit(Mat &mat)
 {
-    //Logger::getLogger()->info("get_current_dir_name()=%s", get_current_dir_name());
+	//Logger::getLogger()->info("get_current_dir_name()=%s", get_current_dir_name());
 	//string model_file("/home/pi/dev/FogLAMP/plugins/south/ImageClassifier/digit_recognition.tflite");
 	string model_file(m_tflite_model);
 	// Load model.
-    auto model = tflite::FlatBufferModel::BuildFromFile(model_file.c_str());
-    if (!model) {
-        Logger::getLogger()->error("Failed to load tensorflow lite model: %s", model_file.c_str());
-        return -1;
-    }
-    // Create interpreter.
-    tflite::ops::builtin::BuiltinOpResolver resolver;
-    std::unique_ptr<tflite::Interpreter> interpreter;
-    tflite::InterpreterBuilder(*model, resolver)(&interpreter);
-    if (!interpreter) {
-        Logger::getLogger()->error("Failed to create TFlite interpreter");
-        return -1;
-    }
-    if (interpreter->AllocateTensors() != kTfLiteOk) {
-        Logger::getLogger()->error("Failed to allocate tensors");
-        return -1;
-    }
-    interpreter->SetNumThreads(1);
-    // Get input / output.
-    const int input = interpreter->inputs()[0];
-    TfLiteTensor* input_tensor = interpreter->tensor(input);
-    TfLiteIntArray* input_dims = input_tensor->dims;
-    const uint32_t width = input_dims->data[1], height = input_dims->data[2];
-	//cout << "input_dims->data[0]=" << input_dims->data[0] << ", input_dims->data[1]=" << input_dims->data[1] << endl;
-	//cout << "input_tensor->type=" << input_tensor->type << ", kTfLiteFloat32=" << kTfLiteFloat32 << ", kTfLiteUInt8=" << kTfLiteUInt8 << endl;
-	//cout << "mat.rows=" << mat.rows << ", mat.cols=" << mat.cols << endl;
+	auto model = tflite::FlatBufferModel::BuildFromFile(model_file.c_str());
+	if (!model) {
+		Logger::getLogger()->error("Failed to load tensorflow lite model: %s", model_file.c_str());
+		return -1;
+	}
+	// Create interpreter.
+	tflite::ops::builtin::BuiltinOpResolver resolver;
+	std::unique_ptr<tflite::Interpreter> interpreter;
+	tflite::InterpreterBuilder(*model, resolver)(&interpreter);
+	if (!interpreter) {
+		Logger::getLogger()->error("Failed to create TFlite interpreter");
+		return -1;
+	}
+	if (interpreter->AllocateTensors() != kTfLiteOk) {
+		Logger::getLogger()->error("Failed to allocate tensors");
+		return -1;
+	}
+	interpreter->SetNumThreads(1);
+	// Get input / output.
+	const int input = interpreter->inputs()[0];
+	TfLiteTensor* input_tensor = interpreter->tensor(input);
+	TfLiteIntArray* input_dims = input_tensor->dims;
+	const uint32_t width = input_dims->data[1], height = input_dims->data[2];
+	//logfile << "input_dims->data[0]=" << input_dims->data[0] << ", input_dims->data[1]=" << input_dims->data[1] << ", input_dims->data[2]=" << input_dims->data[2] << endl;
+	//logfile << "input_tensor->type=" << input_tensor->type << ", kTfLiteFloat32=" << kTfLiteFloat32 << ", kTfLiteUInt8=" << kTfLiteUInt8 << endl;
+	//logfile << "mat.rows=" << mat.rows << ", mat.cols=" << mat.cols << endl;
 	for (int i = 0; i < mat.cols; i++)
 	{
 		input_tensor->data.f[i] = mat.at<float>(0,i);
 	}
-    const int output = interpreter->outputs()[0];
-    TfLiteTensor* output_tensor = interpreter->tensor(output);
+	const int output = interpreter->outputs()[0];
+	TfLiteTensor* output_tensor = interpreter->tensor(output);
 	TfLiteIntArray* output_dims = output_tensor->dims;
-	//cout << "output_dims->data[0]=" << output_dims->data[0] << ", output_dims->data[1]=" << output_dims->data[1] << endl;
-	//cout << "output_tensor->type=" << output_tensor->type << ", kTfLiteFloat32=" << kTfLiteFloat32 << ", kTfLiteUInt8=" << kTfLiteUInt8 << endl;
+	//logfile << "output_dims->data[0]=" << output_dims->data[0] << ", output_dims->data[1]=" << output_dims->data[1] << endl;
+	//logfile << "output_tensor->type=" << output_tensor->type << ", kTfLiteFloat32=" << kTfLiteFloat32 << ", kTfLiteUInt8=" << kTfLiteUInt8 << endl;
 
 	const auto start = std::chrono::high_resolution_clock::now();
-    const TfLiteStatus rc = interpreter->Invoke();
-    const std::chrono::duration<double> duration = std::chrono::high_resolution_clock::now() - start;
+	const TfLiteStatus rc = interpreter->Invoke();
+	const std::chrono::duration<double> duration = std::chrono::high_resolution_clock::now() - start;
 
 	const auto topn = GetTopN(output_tensor, 3);
 
@@ -154,27 +160,28 @@ int ImageClassifier::identifyDigit(Mat &mat)
 /**
  * Get string representation of type of cv::Mat - for debug purpose only
  */
-string type2str(int type) {
-  string r;
+string type2str(int type)
+{
+	string r;
 
-  uchar depth = type & CV_MAT_DEPTH_MASK;
-  uchar chans = 1 + (type >> CV_CN_SHIFT);
+	uchar depth = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
 
-  switch ( depth ) {
-    case CV_8U:  r = "8U"; break;
-    case CV_8S:  r = "8S"; break;
-    case CV_16U: r = "16U"; break;
-    case CV_16S: r = "16S"; break;
-    case CV_32S: r = "32S"; break;
-    case CV_32F: r = "32F"; break;
-    case CV_64F: r = "64F"; break;
-    default:     r = "User"; break;
-  }
+	switch ( depth ) {
+		case CV_8U:  r = "8U"; break;
+		case CV_8S:  r = "8S"; break;
+		case CV_16U: r = "16U"; break;
+		case CV_16S: r = "16S"; break;
+		case CV_32S: r = "32S"; break;
+		case CV_32F: r = "32F"; break;
+		case CV_64F: r = "64F"; break;
+		default:     r = "User"; break;
+	}
 
-  r += "C";
-  r += (chans+'0');
+	r += "C";
+	r += (chans+'0');
 
-  return r;
+	return r;
 }
 
 /**
@@ -188,6 +195,29 @@ void saveImage(Mat &image, string filename)
 
 	imwrite(filename, image, compression_params);
 }
+
+struct contour_LR_sorter // 'less' for contours 
+{
+	bool operator ()( const vector<Point>& a, const vector<Point> & b )
+	{
+		Rect ra(boundingRect(a));
+		Rect rb(boundingRect(b));
+		// scale factor for y should be larger than img.width
+		return ( (ra.x + 3000*ra.y) < (rb.x + 3000*rb.y) );
+	}
+};
+
+struct contour_area_sorter // 'less' for contours - sort by increasing area
+{
+	bool operator ()( const vector<Point>& a, const vector<Point> & b )
+	{
+		double aa = contourArea( a,false);
+		double bb = contourArea( b,false);
+
+		// scale factor for y should be larger than img.width
+		return ( aa < bb );
+	}
+};
 
 /**
  * Process image so that it can be fed into tensorflow model
@@ -203,8 +233,6 @@ int ImageClassifier::processImage(Mat &image)
 	  Logger::getLogger()->error("Could not find image data") ;
 	  return -1;
 	}
-	ofstream logfile;
-  	logfile.open ("logs.txt");
 	Mat src = image; // keep a copy of original image
 	saveImage(image, "image-1.jpg");
 
@@ -227,57 +255,62 @@ int ImageClassifier::processImage(Mat &image)
 	
 	// detect edges using canny
 	Canny( gray, canny_output, 100, 150, 3 );
-    dilate(canny_output, canny_output, kernel);
-    findContours( canny_output, contours, cv::noArray(), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	dilate(canny_output, canny_output, kernel);
+	findContours( canny_output, contours, cv::noArray(), CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
 	
 	saveImage(canny_output, "image-1.5.jpg");
 
 	//cout << "gray: gray.rows=" << gray.rows << ", gray.cols=" << gray.cols << ", area=" << gray.rows*gray.cols << endl;
 	//Logger::getLogger()->info("gray: %d x %d, area=%d", gray.rows, gray.cols, gray.rows*gray.cols);
-
+	
+	// sort contours
+	//std::sort(contours.begin(), contours.end(), contour_area_sorter());
+	
 	Rect bounding_rect;
 	int largest_area=0;
 	int largest_contour_index=0;
 	Scalar color( 255,255,255);  // color of the contour in the
 	int num=1;
 	for( int i = 0; i< contours.size(); i++ )
-    {
-        //  Find the area of contour
-        double a=contourArea( contours[i],false);
-		if (a<300 || a>gray.rows*gray.cols/2) continue;
-		//cout << "Contour area=" << a << ", contours[i].size()=" << contours[i].size() << endl;
+	{
+		//  Find the area of contour
+		double a=contourArea( contours[i],false);
+		if (a<300 || a>gray.rows*gray.cols/4) continue;
+		logfile << "Contour area=" << a << endl; // ", contours[i].size()=" << contours[i].size() << endl;
 		
 		bounding_rect = boundingRect(contours[i]);
 		float ar = (float) bounding_rect.width / bounding_rect.height;
-		if(ar < 1.0/4.0 || ar > 4.0)
+		if(ar < 1.0/10.0 || ar > 10.0)
 		{
-			//cout << "Skipping contour because AR=" << ar << ", bounding_rect.width=" << bounding_rect.width << ", bounding_rect.height=" << bounding_rect.height << endl;
-			continue; // skip if aspect ratio is more than 4 or less than 1/4
+			logfile << "Skipping contour because AR=" << ar << ", bounding_rect.width=" << bounding_rect.width << ", bounding_rect.height=" << bounding_rect.height << endl;
+			continue; // skip if aspect ratio is more than 10 or less than 1/10
 		}
 		
 		string name("image-" + to_string(num) + "-");
 		num++;
-		
+#if 0		
 		RotatedRect rect = minAreaRect( Mat(contours[i]) );
 		
-        Mat M, rotated, cropped;
-        // get angle and size from the bounding box
-        float angle = rect.angle;
-        Size rect_size = rect.size;
+		Mat M, rotated, cropped;
+		// get angle and size from the bounding box
+		float angle = rect.angle;
+		Size rect_size = rect.size;
 		//cout << "angle=" << angle << ", rect_size=" << rect_size << endl;
-        
-        if (rect.angle < -45.0) {
-            angle += 90.0;
-            swap(rect_size.width, rect_size.height);
-        }
-        // get the rotation matrix
-        M = getRotationMatrix2D(rect.center, angle, 1.0);
-        // perform the affine transformation
-        warpAffine(gray, rotated, M, src.size(), INTER_CUBIC);
-        // crop the resulting image
-        getRectSubPix(rotated, rect_size, rect.center, cropped);
+		
+		if (rect.angle < -45.0) {
+			angle += 90.0;
+			swap(rect_size.width, rect_size.height);
+		}
+		// get the rotation matrix
+		M = getRotationMatrix2D(rect.center, angle, 1.0);
+		// perform the affine transformation
+		warpAffine(gray, rotated, M, src.size(), INTER_CUBIC);
+		// crop the resulting image
+		getRectSubPix(rotated, rect_size, rect.center, cropped);
 		saveImage(cropped, name+"2.jpg");
-
+#else
+		Mat cropped = gray(bounding_rect);
+#endif
 		bitwise_not(cropped, image);
 
 		threshold(image, image2, 128, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
@@ -288,7 +321,7 @@ int ImageClassifier::processImage(Mat &image)
 		int rows = image.rows;
 		int cols = image.cols;
 
-		logfile << "bounding rect: image.rows=" << image.rows << ", image.cols=" << image.cols << endl;
+		logfile << "bounding rect: image.rows=" << image.rows << ", image.cols=" << image.cols;
 		
 		double factor;
 		if (rows > cols)
@@ -303,7 +336,7 @@ int ImageClassifier::processImage(Mat &image)
 			cols = 20;
 			rows = int(round(rows*factor));
 		}
-		logfile << "resize to rows=" << rows << ", cols=" << cols << ", factor=" << factor << endl;
+		logfile << " - resize to rows=" << rows << ", cols=" << cols << ", factor=" << factor << endl;
 		if (rows==0 || cols==0) continue;
 		resize(image, image2, Size(cols,rows), 0, 0, INTER_CUBIC);
 		//imshow( "20 x 20 image", image2 );
@@ -361,7 +394,6 @@ int ImageClassifier::processImage(Mat &image)
 		else
 			digit = -1;
 	}
-	logfile.close();
 	return digit;
 }
 
@@ -375,7 +407,7 @@ bool ImageClassifier::takeImage(Mat& mat)
 	if(!cap.isOpened())  // check if we succeeded
 	{
 		Logger::getLogger()->error("Camera 0 is not open");
-        return false;
+		return false;
 	}
 
 	cap >> mat; // get a new image from camera
@@ -407,9 +439,13 @@ Reading	ImageClassifier::takeReading()
 
 	Mat image2;
 	cv::cvtColor(image, image2, CV_BGR2GRAY);
+	
+	logfile.open ("/tmp/logs.txt");
 
 	long digit = processImage(image2);
 	//Logger::getLogger()->info("I think digit is %d, prob=%f", rv2, topn_prob);
+
+	logfile.close();
 
 	if (digit != -1)
 	{
